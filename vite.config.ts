8aaeac22f887192169086e99a2e8ce9b029df8b8
Gareth Williams <gaz492@gmail.com>
2023-10-15 00:26:21 +0100

    now sha?

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const commitSha = process.env.CF_PAGES_COMMIT_SHA
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    VITE_APP_COMMIT_SHA: JSON.stringify(commitSha)
+  },
   plugins: [react()],
 })
